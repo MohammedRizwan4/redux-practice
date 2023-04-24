8cf21489fac5d135ad9744d55e7eba833b83fa5e
MohammedRizwan4 <rizwan.bhavnagri0411@gmail.com>
2023-04-25 00:08:08 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './container/Header';
+import { Route, Routes } from 'react-router-dom';
+import ProductListing from './container/ProductListing';
+import ProductDetails from './container/ProductDetails';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<ProductListing />}></Route>
+        <Route path='/product/:productId' element={<ProductDetails />}></Route>
+        <Route>404 NOT FOUND</Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/container/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css'
+
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>FakeShop</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
